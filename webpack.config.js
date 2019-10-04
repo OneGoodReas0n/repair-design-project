90cbd4d24d4b7a81dde98baf53b73051aa2abe3c
OneGoodReas0n <korobovi4@gmail.com>
2019-10-04 18:45:25 +0300

    Implementation Readme.md

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,46 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './scripts/index.js',
 
     output: {
-        path: path.resolve(__dirname,'dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader:"css-loader",
+                    },
+                    {
+                        loader:"sass-loader",
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        })
+    ],
 
     mode: 'development'
 }
