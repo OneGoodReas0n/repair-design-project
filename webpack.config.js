ab7b392a0e9ae4b33869cd7b7d34c18ec5e1a86d
OneGoodReas0n <korobovi4@gmail.com>
2019-12-10 20:46:58 +0200

    update: add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,59 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './scripts/index.js',
+  entry: "./scripts/index.js",
 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                    {
-                        loader: "sass-loader",
-                    }
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ['file-loader'],
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [{ loader: 'url-loader?limit=100000' }]
-            }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
         ]
-    },
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [{ loader: "url-loader?limit=100000" }]
+      }
+    ]
+  },
 
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "style.css"
-        })
-    ],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    }),
 
-    mode: 'development'
-}
+    new HtmlWebpackPlugin()
+  ],
+
+  mode: "development"
+};
